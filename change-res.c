27f9715bdc09806d3242654eee336e1f314106e5
MarlonWSantos <marlon.santos.santos@icen.ufpa.br>
2020-03-08 23:36:17 -0300

    Adicionado rest engine e URL para o resource res_range_events

--- change-res.c
+++ change-res.c
@@ -1,24 +1,32 @@
 #include "contiki.h"
 #include <stdio.h> 
+#include "resources/res-range-events.h"
+#include "rest-engine.h"
 
-
-unsigned int range=1;
-
+extern resource_t res_range_events;
 
 /*---------------------------------------------------------------------------*/
 PROCESS(change_res_process, "Change-res");
 AUTOSTART_PROCESSES(&change_res_process);
 /*---------------------------------------------------------------------------*/
-
+  
 
 PROCESS_THREAD(change_res_process, ev, data)
 {
 	PROCESS_BEGIN();
+
+  	rest_init_engine();
+  	rest_activate_resource(&res_range_events,"res_range_events");
+
+	range=10;
+	count=1;
+
 	static struct etimer et;
 	while(1) {
 		etimer_set(&et, CLOCK_SECOND*5);
 		PROCESS_WAIT_EVENT();
-		printf("Range: %d\n", range);
+		printf("[%d]Range: %d\n",count,range);
+		count++;
 		if(etimer_expired(&et)) {
 			etimer_reset(&et);
 		}
